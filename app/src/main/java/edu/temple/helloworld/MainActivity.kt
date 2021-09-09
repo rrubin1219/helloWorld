744e3bf663e7dbf16db7ebdda0a3df63fa245beb
Rachel <tul52326@temple.edu>
2021-09-09 16:17:35 -0400

    Added button function and new message

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,10 +2,14 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener{(findViewById<TextView>(R.id.textView)).text = "You clicked the button"}
     }
 }
